3488e86bb5503bd808a89d7c36ae92d2bb784e82
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-03-01 15:38:55 -0600

    progress, stuck on 4.3

--- decision_trees.py
+++ decision_trees.py
@@ -4,18 +4,13 @@ import matplotlib.pyplot as plt
 from utils import load_titanic
 
 class DecisionTree:
-    def __init__(self, max_depth):
-        self.max_depth = max_depth
+    def __init__(self):
         self.root = None
 
-class Node:
+class Leaf:
     def __init__(self, data, depth, max_depth):
         self.data = data
         self.depth = depth
         self.max_depth = max_depth
         self.left = None
         self.right = None
-
-if __name__ == "__main__":
-    titanic_dataframe = load_titanic()
-    print(titanic_dataframe)
--- utils.py
+++ utils.py
@@ -46,3 +46,102 @@ def transform_features(data: pd.DataFrame) -> pd.DataFrame:
         lambda x: True if x >= 34.30542 else False)
     
     return data
+
+def mutual_information(x: np.ndarray, y: np.ndarray) -> float:
+    '''
+    Computes the mutual information between two random variables X and Y.
+    X: Feature variable
+    y: Target variable (survived or not)
+    '''
+    N = len(x)
+    
+    # Count occurrences for each combination of x and y
+    joint_counts = np.zeros((2, 2))
+    for i in range(N):
+        joint_counts[x[i], y[i]] += 1
+
+    # Compute probabilities
+    joint_probs = joint_counts / N
+    marginal_x = np.sum(joint_probs, axis=1)
+    marginal_y = np.sum(joint_probs, axis=0)
+
+    # Compute mutual information using the definition: 
+    #I(xj, y) = sum(p(xj, y) * log2(p(xj, y) / (p(xj) * p(y))))
+    mutual_information = 0
+    for i in range(2):
+        for j in range(2):
+            if joint_probs[i, j] > 0:
+                mutual_information += joint_probs[i, j] * np.log2(joint_probs[i, j] / 
+                                                                  (marginal_x[i] * marginal_y[j]))
+
+    return mutual_information
+
+
+from enum import Enum
+
+class Tree():
+    def __init__(self, features: np.ndarray, target: np.ndarray):
+        self.features = features
+        self.target = target
+        self.tree = self.sort_features_by_mutual_information()
+
+    def sort_features_by_mutual_information(self) -> np.ndarray:
+        '''
+        Sorts the features by mutual information with the target variable.
+        '''
+        mutual_informations = []
+
+        transform = lambda x: 1 if x == True else 0
+        for i in range(len(self.features[1])):
+            transformed_numerical = []
+            for j in self.features[:,i]:
+                transformed_numerical.append(transform(j))
+            mutual_informations.append((i, mutual_information(transformed_numerical, self.target)))
+        
+        return mutual_informations
+
+    def print_tree(self) -> None:
+        '''
+        Prints the tree.
+        '''
+        tree = sorted(self.tree, key=lambda x: x[1], reverse=True)
+        for category, I in tree:
+            category = FeatureType(category)
+            print(f'{category}: {I}')
+    
+    def build_tree(self) -> np.ndarray:
+        '''
+        Builds a decision tree using the mutual information of the features.
+        '''
+        
+    
+class FeatureType(Enum):
+    PassengerClass = 0
+    Gender = 1
+    Age = 2
+    SiblingsSpousesAboard = 3
+    ParentsChildrenAboard = 4
+    Fare = 5
+
+# def mutual_information_estimation(X: np.ndarray, Y: np.ndarray, bins: int = 10) -> float:
+#     '''
+#     Estimates the mutual information between two random variables X and Y.
+#     '''
+#     #Create a 2D histogram of the two variables
+#     H, x_edges, y_edges = np.histogram2d(X, Y, bins = bins)
+    
+#     #Compute the joint probability distribution
+#     Pxy = H / float(np.sum(H))
+    
+#     #Compute the marginal probability distributions
+#     Px = np.sum(Pxy, axis = 1)
+#     Py = np.sum(Pxy, axis = 0)
+    
+#     #Compute the mutual information
+#     I = 0
+#     for i in range(Pxy.shape[0]):
+#         for j in range(Pxy.shape[1]):
+#             if Pxy[i, j] > 0:
+#                 I += Pxy[i, j] * np.log(Pxy[i, j] / (Px[i] * Py[j]))
+#     return I
+    
